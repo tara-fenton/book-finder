829b7250168e0c20204a80f3890294ba17ec20a3
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2019-03-03 22:25:30 -0500

    components render api data

--- src/App.js
+++ src/App.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
 import BookList from './components/BookList';
 import './App.css';
+import axios from 'axios';
 
+const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books: [],
+      dataLoaded: false
+    }
+  }
+
+  async fetchBookData() {
+    const query = 'quilting';
+    const resp = await axios.get(`${BASE_URL}${query}`);
+    this.setState({ books: resp.data,  dataLoaded: true });
+    console.log("fetch books ",this.state.books)
+    // this.filterResults();
+    return resp.data;
+  }
+  filterResults() {
+    this.state.books.items.map(book => {
+      console.log(book.volumeInfo.publisher);
+    })
+  }
+  async componentDidMount() {
+    await this.fetchBookData();
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+
         </header>
         <main>
           <SearchBar />
-          <BookList />
+          <BookList books={this.state.books} dataLoaded={this.state.dataLoaded} />
         </main>
       </div>
     );
--- src/components/BookList/index.js
+++ src/components/BookList/index.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 import BookItem from '../BookItem'
 
 export default class BookList extends Component {
+  
   render() {
     return (
       <div>
-        <BookItem />
+        {this.props.dataLoaded ? (
+          this.props.books.items.map(book => {
+            return <BookItem book={book} key={book.id} />
+          })
+        ) : (
+            <h2>Loading</h2>
+          )}
+
       </div>
     )
   }
