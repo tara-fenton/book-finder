a4d393cfc672a30b1cfa4821c6e79baddc81ab9e
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2019-03-05 00:44:10 -0500

    basic styles

--- src/components/BookItem/index.js
+++ src/components/BookItem/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import './index.css'
 
 const BookItem = ({ book }) => {
 
   return (
-    <div>
+    <div className="book-item">
         <h3>{book.volumeInfo.title}</h3>
         <h6>Authors: {book.volumeInfo.authors.join(", ")}</h6>
         <h6>Publisher: {book.volumeInfo.publisher}</h6>
+        <div className="book-info">
         <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
         <a href={book.volumeInfo.infoLink}>More Info</a>
+        </div>
       </div>
   );
 };
--- src/components/BookList/index.js
+++ src/components/BookList/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import BookItem from '../BookItem'
+import './index.css'
 
 export default class BookList extends Component {
   
   render() {
     return (
-      <div>
+      <div className="book-list">
         {this.props.dataLoaded ? (
           this.props.books.items.map(book => {
             return <BookItem book={book} key={book.id} />
